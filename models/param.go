2a077665d20cd29b57a4fec5b8f2d6b036ada2ec
xiaoming325 <141613927+xiaoming325@users.noreply.github.com>
2024-11-28 20:48:35 +0800

    基本功能全部完成

--- models/param.go
+++ models/param.go
@@ -25,7 +25,7 @@ type LoginParam struct {
 type VoteData struct {
 	//UserID 从请求中获取当前的用户， 不用在结构体中添加
 	PostID    string `json:"post_id" bind:"required"`              //帖子id
-	Direction int8   `json:"direction,string" bind:"oneof=-1 0 1"` //赞成票(1)or反对票(-1)or取消投票(0) TODO这里oneof不生效
+	Direction int8   `json:"direction,string" bind:"oneof=-1 0 1"` //赞成票(1)or反对票(-1)or取消投票(0)
 	//oneof=-1,0,1表示该字段的值要求只可能是-1或0或1
 }
 
